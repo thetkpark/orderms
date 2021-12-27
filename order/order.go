2f07d0d7962efa9c894ff99d7b1550ce8e07e731
Sethanant Pipatpakorn <the_tkpark@hotmail.com>
2021-12-27 15:57:59 +0700

    finish tdd design on acceptOnlineOnly

--- order/order.go
+++ order/order.go
@@ -1,52 +1,14 @@
 package order
 
-import (
-	"fmt"
-	"net/http"
-)
-
-type storer interface {
-	Save(Order) error
-}
-
 type Context interface {
 	Order() (Order, error)
-	JSON(int, interface{})
+	JSON(code int, v interface{})
 }
 
 type Handler struct {
-	store  storer
-	filter string
-}
-
-func NewHandler(store storer, filter string) *Handler {
-	return &Handler{store: store, filter: filter}
+	channel string
 }
 
 func (h *Handler) Order(c Context) {
-	order, err := c.Order()
-	if err != nil {
-		c.JSON(http.StatusBadRequest, map[string]string{
-			"error": err.Error(),
-		})
-		return
-	}
 
-	if order.SalesChannel != h.filter {
-		c.JSON(http.StatusNoContent, map[string]string{
-			"message": fmt.Sprintf("%s is not accept", order.SalesChannel),
-		})
-		return
-	}
-
-	if err := h.store.Save(order); err != nil {
-		c.JSON(http.StatusInternalServerError, map[string]string{
-			"error": err.Error(),
-		})
-		return
-	}
-
-	c.JSON(http.StatusOK, map[string]string{
-		"message": fmt.Sprintf("%d is saved", order.OrderID),
-	})
 }
