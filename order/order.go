b9cdc5141de8740a3dec6ff77ec9cf1cb50a673e
Sethanant Pipatpakorn <the_tkpark@hotmail.com>
2021-12-27 16:00:24 +0700

    make the test TestOnlyAcceptOnlineChannel pass

--- order/order.go
+++ order/order.go
@@ -1,5 +1,7 @@
 package order
 
+import "net/http"
+
 type Context interface {
 	Order() (Order, error)
 	JSON(code int, v interface{})
@@ -10,5 +12,14 @@ type Handler struct {
 }
 
 func (h *Handler) Order(c Context) {
+	order, err := c.Order()
+	if err != nil {
 
+	}
+
+	if order.SalesChannel != h.channel {
+		c.JSON(http.StatusBadRequest, map[string]string{
+			"message": "Offline is not accepted",
+		})
+	}
 }
