e70a477c0a246110378db93bf9f22ea922d31376
Sethanant Pipatpakorn <the_tkpark@hotmail.com>
2021-12-29 23:25:04 +0700

    add Router and POST method to it

--- router/context.go
+++ router/context.go
@@ -5,6 +5,19 @@ import (
 	"github.com/pallat/micro/order"
 )
 
+// HandlerFunc defined new type for handler func
+type HandlerFunc func(ctx order.Context)
+
+type Router struct {
+	*gin.Engine
+}
+
+func (r *Router) POST(path string, handler HandlerFunc) {
+	r.Engine.POST(path, func(context *gin.Context) {
+		handler(&Context{context})
+	})
+}
+
 type Context struct {
 	*gin.Context
 }
